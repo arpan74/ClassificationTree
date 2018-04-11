a1703b6e5da51c9d403d8eff4bb30e9e5c5875fe
Arpan Gupta <arpan74@gmail.com>
2018-04-10 20:17:33 -0400

    Added a function to create a split based on attribute and added a function to check where best split location is based on gini index

--- decisionTree.py
+++ decisionTree.py
@@ -57,3 +57,23 @@ def entropyCalc(groups, classes):
         entropy += score
     return entropy
 
+def test_split(index, value, dataset): # left is no, right is yes
+	left, right = list(), list()
+	for row in dataset:
+		if row[index] < value:
+			left.append(row)
+		else:
+			right.append(row)
+	return left, right
+
+# Select the best split point for a dataset
+def get_split(dataset):
+	class_values = list(set(row[-1] for row in dataset))
+	b_index, b_value, b_score, b_groups = 999, 999, 999, None
+	for index in range(len(dataset[0])-1):
+		for row in dataset:
+			groups = test_split(index, row[index], dataset)
+			gini = gini_index(groups, class_values) # Can also use entropy
+			if gini < b_score:
+				b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+	return {'index':b_index, 'value':b_value, 'groups':b_groups}
